d769e9f00acc2f334fc9455bdb3f9f9507e0152e
Marko <markogajiin@gmail.com>
2020-01-11 22:38:28 +0100

    static files added

--- repertoer/settings.py
+++ repertoer/settings.py
@@ -57,7 +57,8 @@ ROOT_URLCONF = 'repertoer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # import templates
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,4 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS= [
+    os.path.join(BASE_DIR, 'repertoer/static')
+] 
