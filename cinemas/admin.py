9b0f7cf92f2b7625a91f8a28ed04b57023e49de4
Marko <markogajiin@gmail.com>
2020-01-14 00:44:23 +0100

    Admin display data customization

--- cinemas/admin.py
+++ cinemas/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Cinema
 
-admin.site.register(Cinema)
+
+# Filtering
+class CinemaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'address', 'city')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', )
+    list_per_page = 20
+
+admin.site.register(Cinema, CinemaAdmin)
--- movies/admin.py
+++ movies/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import Movie
 
-admin.site.register(Movie)
+# Filtering
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'genre', 'rating')
+    list_dispplay_links = ('id', 'title')
+    list_filter = ('cinema',)
+    # list_editable = ('is_published',)
+    search_fields = ('title', 'genre')
+    list_per_page = 20
+
+admin.site.register(Movie, MovieAdmin)
