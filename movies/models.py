2ba9460fc382147c01b707786781e1e6498c811d
Marko <markogajiin@gmail.com>
2020-01-13 21:44:00 +0100

    Movie model implemented

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from cinemas.models import Cinema
 
-# Create your models here.
+class Movie(models.Model):
+    cinema = models.ForeignKey(Cinema, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+    description = models.CharField(blank=True, max_length=200)
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    released = models.IntegerField()
+    runtime = models.IntegerField()
+    ticket_price = models.IntegerField()
+    poster = models.ImageField(upload_to='posters/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True)
+    def __str__(self):
+        return self.title
