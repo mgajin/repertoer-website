1dc1a742ac0db3fbc2da2e3e2caa7b3aa03ed1ca
Marko <markogajiin@gmail.com>
2020-01-13 21:55:09 +0100

    Cinema model implemented

--- cinemas/models.py
+++ cinemas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Cinema(models.Model):
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=20)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    phone = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
