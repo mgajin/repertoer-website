786c80f5fb4d98ca0c7c984f1b64ecf2da44e5ad
Marko <markogajiin@gmail.com>
2020-01-13 23:18:04 +0100

    Media folder implementation

--- repertoer/settings.py
+++ repertoer/settings.py
@@ -130,3 +130,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS= [
     os.path.join(BASE_DIR, 'repertoer/static')
 ] 
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
