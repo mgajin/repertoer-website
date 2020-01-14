a534d80f1f4fa5a019eff92e84b6e5d4f3e5830b
Marko <markogajiin@gmail.com>
2020-01-14 16:41:21 +0100

    get movies by released data & rating implementation

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from movies.models import Movie 
+
 def index(request):
-    return render(request, 'pages/index.html')
+
+    latest_movies = Movie.objects.order_by('-released')[:5]
+    top_rated = Movie.objects.order_by('-rating')[:6]
+
+
+    context = {
+        'latest_movies': latest_movies,
+        'top_rated': top_rated
+    }
+
+    return render(request, 'pages/index.html', context)
 
 def repertoer(request):
     return render(request, 'pages/repertoer.html')
