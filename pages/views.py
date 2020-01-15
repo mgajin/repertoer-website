0eeff81dbce82f5c95ce316c8e18a668129cea47
Marko <markogajiin@gmail.com>
2020-01-15 22:36:44 +0100

    repertoer filter implementation

--- pages/views.py
+++ pages/views.py
@@ -19,14 +19,27 @@ def index(request):
 
 def repertoer(request):
 
+    queryset_list = Movie.objects.order_by('title')
+
+    # Search movie by title
+    if 'title' in request.GET:
+        title = request.GET['title']
+        if title:
+            queryset_list = queryset_list.filter(title__icontains=title)
+
+    # Filter movies by genre 
+    if 'genre' in request.GET:
+        genre = request.GET['genre']
+        if genre != 'All':
+            queryset_list = queryset_list.filter(genre__icontains=genre)
+
     cinemas = Cinema.objects.all()
     genres = ['All', 'Action', 'Comedy', 'Drama', 'Sci-Fi', 'Thriller', 'Crime']
-    movies = Movie.objects.all()
 
     context = {
         'cinemas': cinemas,
         'genres': genres,
-        'movies': movies
+        'movies': queryset_list
     }
 
     return render(request, 'pages/repertoer.html', context)
